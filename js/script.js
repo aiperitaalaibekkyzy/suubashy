ea366eda51967f94ca6ed6e2a91bc7b5124a1e2f
Peri <aiperitaalaibekkyzy@gmail.com>
2022-04-16 14:14:56 +0600

    navbar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 // Look for .hamburger
 var hamburger = document.querySelector(".hamburger");
+var nav = document.querySelector(".header_nav ");
 // On click
 hamburger.addEventListener("click", function() {
   // Toggle class "is-active"
   hamburger.classList.toggle("is-active");
+  nav.classList.toggle("is-active");
   // Do something else, like open/close menu
 });
+
